258d86a1b5c90004cf2f0d5c13324a4c3cca0f6e
MCUdude <mcudude@users.noreply.github.com>
2015-11-19 10:44:36 +0100

    Uploaded core files

---
+++ avr/bootloaders/optiboot/baudcheck.tmp.sh
@@ -0,0 +1,31 @@
+# 1 "baudcheck.c"
+# 1 "/Users/hansi/Documents/Arduino/hardware/DIP-40/avr/bootloaders/optiboot//"
+# 1 "<command-line>"
+# 1 "baudcheck.c"
+# 24 "baudcheck.c"
+bpsx=9600
+bps=${bpsx/L/}
+fcpux=1000000L
+fcpu=${fcpux/L/}
+
+
+
+
+
+BAUD_SETTING=$(( ( ($fcpu + $bps * 4) / (($bps * 8))) - 1 ))
+
+
+
+
+
+
+
+BAUD_ACTUAL=$(( ($fcpu/(8 * (($BAUD_SETTING)+1))) ))
+BAUD_ERROR=$(( (( 100*($bps - $BAUD_ACTUAL) ) / $bps) ))
+ERR_TS=$(( ((( 1000*($bps - $BAUD_ACTUAL) ) / $bps) - $BAUD_ERROR * 10) ))
+ERR_TENTHS=$(( ERR_TS > 0 ? ERR_TS: -ERR_TS ))
+
+
+
+
+echo BAUD RATE CHECK: Desired: $bps, Real: $BAUD_ACTUAL, UBRRL = $BAUD_SETTING, Error=$BAUD_ERROR.$ERR_TENTHS\%
---
+++ avr/variants/standard1284/pins_arduino.h
@@ -0,0 +1,217 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <avr/pgmspace.h>
+
+// ATMEL ATMEGA1284P
+//
+//   	 
+//	 	                 +---\/---+
+//       LED (D 0) PB0  1|        |40  PA0 (AI 0 / D24)
+//           (D 1) PB1  2|        |39  PA1 (AI 1 / D25)
+//      INT2 (D 2) PB2  3|        |38  PA2 (AI 2 / D26)
+//       PWM (D 3) PB3  4|        |37  PA3 (AI 3 / D27)
+//    PWM/SS (D 4) PB4  5|        |36  PA4 (AI 4 / D28)
+//      MOSI (D 5) PB5  6|        |35  PA5 (AI 5 / D29)
+//  PWM/MISO (D 6) PB6  7|        |34  PA6 (AI 6 / D30)
+//   PWM/SCK (D 7) PB7  8|        |33  PA7 (AI 7 / D31)
+//                 RST  9|        |32  AREF
+//                 VCC 10|        |31  GND 
+//                 GND 11|        |30  AVCC
+//               XTAL2 12|        |29  PC7 (D 23)
+//               XTAL1 13|        |28  PC6 (D 22)
+//      RX0 (D 8)  PD0 14|        |27  PC5 (D 21) TDI
+//      TX0 (D 9)  PD1 15|        |26  PC4 (D 20) TDO
+// RX1/INT0 (D 10) PD2 16|        |25  PC3 (D 19) TMS
+// TX1/INT1 (D 11) PD3 17|        |24  PC2 (D 18) TCK
+//      PWM (D 12) PD4 18|        |23  PC1 (D 17) SDA
+//      PWM (D 13) PD5 19|        |22  PC0 (D 16) SCL
+//      PWM (D 14) PD6 20|        |21  PD7 (D 15) PWM
+//                   	 +--------+
+//
+
+/*
+   PCINT15-8: D7-0  : bit 1
+   PCINT31-24: D15-8  : bit 3
+   PCINT23-16: D23-16 : bit 2
+   PCINT7-0: D31-24   : bit 0
+*/
+
+#define NUM_DIGITAL_PINS            31
+#define NUM_ANALOG_INPUTS           8
+#define analogInputToDigitalPin(p)  ((p < NUM_ANALOG_INPUTS) ? (p) + 24 : -1)
+
+#define digitalPinHasPWM(p)         ((p) == 3 || (p) == 4 || (p) == 6 || (p) == 7 || (p) == 12 || (p) == 13 || (p) == 14 || (p) == 15)
+
+static const uint8_t SS   = 4;
+static const uint8_t MOSI = 5;
+static const uint8_t MISO = 6;
+static const uint8_t SCK  = 7;
+
+static const uint8_t SDA = 17;
+static const uint8_t SCL = 16;
+static const uint8_t LED = 0;
+
+static const uint8_t A0 = 24;
+static const uint8_t A1 = 25;
+static const uint8_t A2 = 26;
+static const uint8_t A3 = 27;
+static const uint8_t A4 = 28;
+static const uint8_t A5 = 29;
+static const uint8_t A6 = 30;
+static const uint8_t A7 = 31;
+
+#define digitalPinToPCICR(p)    (((p) >= 0 && (p) < NUM_DIGITAL_PINS) ? (&PCICR) : ((uint8_t *)0))
+#define digitalPinToPCICRbit(p) (((p) <= 7) ? 1 : (((p) <= 15) ? 3 : (((p) <= 23) ? 2 : 0)))
+#define digitalPinToPCMSK(p)    (((p) <= 7) ? (&PCMSK2) : (((p) <= 13) ? (&PCMSK0) : (((p) <= 21) ? (&PCMSK1) : ((uint8_t *)0))))
+#define digitalPinToPCMSKbit(p) ((p) % 8)
+
+#ifdef ARDUINO_MAIN
+
+#define PA 1
+#define PB 2
+#define PC 3
+#define PD 4
+
+// these arrays map port names (e.g. port B) to the
+// appropriate addresses for various functions (e.g. reading
+// and writing)
+const uint16_t PROGMEM port_to_mode_PGM[] =
+{
+	NOT_A_PORT,
+	(uint16_t) &DDRA,
+	(uint16_t) &DDRB,
+	(uint16_t) &DDRC,
+	(uint16_t) &DDRD,
+};
+
+const uint16_t PROGMEM port_to_output_PGM[] =
+{
+	NOT_A_PORT,
+	(uint16_t) &PORTA,
+	(uint16_t) &PORTB,
+	(uint16_t) &PORTC,
+	(uint16_t) &PORTD,
+};
+
+const uint16_t PROGMEM port_to_input_PGM[] =
+{
+	NOT_A_PORT,
+	(uint16_t) &PINA,
+	(uint16_t) &PINB,
+	(uint16_t) &PINC,
+	(uint16_t) &PIND,
+};
+
+const uint8_t PROGMEM digital_pin_to_port_PGM[] =
+{
+	PB, /* 0 */
+	PB,
+	PB,
+	PB,
+	PB,
+	PB,
+	PB,
+	PB,
+	PD, /* 8 */
+	PD,
+	PD,
+	PD,
+	PD,
+	PD,
+	PD,
+	PD,
+	PC, /* 16 */
+	PC,
+	PC,
+	PC,
+	PC,
+	PC,
+   	PC,
+	PC,
+	PA, /* 24 */
+	PA,
+	PA,
+	PA,
+	PA,
+	PA,
+	PA,
+	PA  /* 31 */
+};
+
+const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] =
+{
+	_BV(0), /* 0, port B */
+	_BV(1),
+	_BV(2),
+	_BV(3),
+	_BV(4),
+	_BV(5),
+	_BV(6),
+	_BV(7),
+	_BV(0), /* 8, port D */
+	_BV(1),
+	_BV(2),
+	_BV(3),
+	_BV(4),
+	_BV(5),
+	_BV(6),
+	_BV(7),
+	_BV(0), /* 16, port C */
+	_BV(1),
+	_BV(2),
+	_BV(3),
+	_BV(4),
+	_BV(5),
+	_BV(6),
+	_BV(7),
+	_BV(0), /* 24, port A */
+	_BV(1),
+	_BV(2),
+	_BV(3),
+	_BV(4),
+	_BV(5),
+	_BV(6),
+	_BV(7)
+};
+
+const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
+{
+	NOT_ON_TIMER, 	/* 0  - PB0 */
+	NOT_ON_TIMER, 	/* 1  - PB1 */
+	NOT_ON_TIMER, 	/* 2  - PB2 */
+	TIMER0A,     	/* 3  - PB3 */
+	TIMER0B, 		/* 4  - PB4 */
+	NOT_ON_TIMER, 	/* 5  - PB5 */
+	TIMER3A, 		/* 6  - PB6 */
+	TIMER3B,		/* 7  - PB7 */
+	NOT_ON_TIMER, 	/* 8  - PD0 */
+	NOT_ON_TIMER, 	/* 9  - PD1 */
+	NOT_ON_TIMER, 	/* 10 - PD2 */
+	NOT_ON_TIMER, 	/* 11 - PD3 */
+	TIMER1B,     	/* 12 - PD4 */
+	TIMER1A,     	/* 13 - PD5 */
+	TIMER2B,     	/* 14 - PD6 */
+	TIMER2A,     	/* 15 - PD7 */
+	NOT_ON_TIMER, 	/* 16 - PC0 */
+	NOT_ON_TIMER,   /* 17 - PC1 */
+	NOT_ON_TIMER,   /* 18 - PC2 */
+	NOT_ON_TIMER,   /* 19 - PC3 */
+	NOT_ON_TIMER,   /* 20 - PC4 */
+	NOT_ON_TIMER,   /* 21 - PC5 */
+	NOT_ON_TIMER,   /* 22 - PC6 */
+	NOT_ON_TIMER,   /* 23 - PC7 */
+	NOT_ON_TIMER,   /* 24 - PA0 */
+	NOT_ON_TIMER,   /* 25 - PA1 */
+	NOT_ON_TIMER,   /* 26 - PA2 */
+	NOT_ON_TIMER,   /* 27 - PA3 */
+	NOT_ON_TIMER,   /* 28 - PA4 */
+	NOT_ON_TIMER,   /* 29 - PA5 */
+	NOT_ON_TIMER,   /* 30 - PA6 */
+	NOT_ON_TIMER    /* 31 - PA7 */
+};
+
+#endif // ARDUINO_MAIN
+
+#endif // Pins_Arduino_h
+// vim:ai:cin:sts=2 sw=2 ft=cpp
