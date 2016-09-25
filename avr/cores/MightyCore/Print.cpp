da3c955024cc2b0f41f57799b22d5414c68ed264
MCUdude <mcudude@users.noreply.github.com>
2016-09-25 13:24:22 +0200

    Core update

--- avr/cores/MightyCore/Print.cpp
+++ avr/cores/MightyCore/Print.cpp
@@ -256,7 +256,7 @@ size_t Print::printFloat(double number, uint8_t digits)
   while (digits-- > 0)
   {
     remainder *= 10.0;
-    int toPrint = int(remainder);
+    unsigned int toPrint = (unsigned int)(remainder);
     n += print(toPrint);
     remainder -= toPrint; 
   } 
