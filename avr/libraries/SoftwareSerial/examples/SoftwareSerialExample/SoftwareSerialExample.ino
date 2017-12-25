8090c40b5e4f8eb3acd875327f7acc6b196453d3
MCUdude <mcudude@users.noreply.github.com>
2017-12-25 15:51:21 +0100

    Update examples

--- avr/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ avr/libraries/SoftwareSerial/examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -64,26 +64,28 @@
 
 SoftwareSerial mySerial(10, 11); // RX, TX
 
-void setup() {
+void setup() 
+{
   // Open serial communications and wait for port to open:
   Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-
+  while (!Serial); // wait for serial port to connect. Needed for native USB port only 
 
   Serial.println("Goodnight moon!");
 
-  // set the data rate for the SoftwareSerial port
+  // Set the data rate for the SoftwareSerial port
   mySerial.begin(4800);
   mySerial.println("Hello, world?");
 }
 
-void loop() { // run over and over
-  if (mySerial.available()) {
+void loop() 
+{ 
+  if (mySerial.available()) 
+  {
     Serial.write(mySerial.read());
   }
-  if (Serial.available()) {
+  
+  if (Serial.available()) 
+  {
     mySerial.write(Serial.read());
   }
 }
