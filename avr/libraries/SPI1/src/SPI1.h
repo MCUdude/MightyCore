1f8167435c38c7e9d3d89597204b91350932d51a
MCUdude <mcudude@users.noreply.github.com>
2018-06-03 13:16:00 +0200

    Add SPI1 support for ATmega324PB SPI1.h is automatically included when ATmega324PB is selected in the boards menu and SPI.h is included. SPI1.h should therefore not be included in the sketch

--- avr/libraries/SPI1/src/SPI1.h
+++ avr/libraries/SPI1/src/SPI1.h
@@ -11,14 +11,10 @@
  * published by the Free Software Foundation.
  */
 
-#ifndef _SPI_H_INCLUDED
-#define _SPI_H_INCLUDED
+#ifndef _SPI1_H_INCLUDED
+#define _SPI1_H_INCLUDED
 
 #include <Arduino.h>
-
-#if defined(SPI1_STC_vect)
-  #include <SPI1.h>
-#endif  
 
 // SPI_HAS_TRANSACTION means SPI has beginTransaction(), endTransaction(),
 // usingInterrupt(), and SPISetting(clock, bitOrder, dataMode)
@@ -40,9 +36,6 @@
 // on if any mismatch is ever detected.
 //#define SPI_TRANSACTION_MISMATCH_LED 5
 
-
-
-
 #ifndef LSBFIRST
 #define LSBFIRST 0
 #endif
@@ -63,9 +56,9 @@
 #define SPI_MODE2 0x08
 #define SPI_MODE3 0x0C
 
-#define SPI_MODE_MASK 0x0C  // CPOL = bit 3, CPHA = bit 2 on SPCR
-#define SPI_CLOCK_MASK 0x03  // SPR1 = bit 1, SPR0 = bit 0 on SPCR
-#define SPI_2XCLOCK_MASK 0x01  // SPI2X = bit 0 on SPSR
+#define SPI_MODE_MASK 0x0C  // CPOL = bit 3, CPHA = bit 2 on SPCR1
+#define SPI_CLOCK_MASK 0x03  // SPR1 = bit 1, SPR0 = bit 0 on SPCR1
+#define SPI_2XCLOCK_MASK 0x01  // SPI2X = bit 0 on SPSR1
 
 // define SPI_AVR_EIMSK for AVR boards with external interrupt pins
 #if defined(EIMSK)
@@ -76,16 +69,16 @@
   #define SPI_AVR_EIMSK  GIMSK
 #endif
 
-class SPISettings {
+class SPI1Settings {
 public:
-  SPISettings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
+  SPI1Settings(uint32_t clock, uint8_t bitOrder, uint8_t dataMode) {
     if (__builtin_constant_p(clock)) {
       init_AlwaysInline(clock, bitOrder, dataMode);
     } else {
       init_MightInline(clock, bitOrder, dataMode);
     }
   }
-  SPISettings() {
+  SPI1Settings() {
     init_AlwaysInline(4000000, MSBFIRST, SPI_MODE0);
   }
 private:
@@ -149,18 +142,18 @@ private:
     // Invert the SPI2X bit
     clockDiv ^= 0x1;
 
-    // Pack into the SPISettings class
-    spcr = _BV(SPE) | _BV(MSTR) | ((bitOrder == LSBFIRST) ? _BV(DORD) : 0) |
+    // Pack into the SPI1Settings class
+    spcr1 = _BV(SPE) | _BV(MSTR) | ((bitOrder == LSBFIRST) ? _BV(DORD) : 0) |
       (dataMode & SPI_MODE_MASK) | ((clockDiv >> 1) & SPI_CLOCK_MASK);
-    spsr = clockDiv & SPI_2XCLOCK_MASK;
+    spsr1 = clockDiv & SPI_2XCLOCK_MASK;
   }
-  uint8_t spcr;
-  uint8_t spsr;
-  friend class SPIClass;
+  uint8_t spcr1;
+  uint8_t spsr1;
+  friend class SPI1Class;
 };
 
 
-class SPIClass {
+class SPI1Class {
 public:
   // Initialize the SPI library
   static void begin();
@@ -182,7 +175,7 @@ public:
   // Before using SPI.transfer() or asserting chip select pins,
   // this function is used to gain exclusive access to the SPI bus
   // and configure the correct settings.
-  inline static void beginTransaction(SPISettings settings) {
+  inline static void beginTransaction(SPI1Settings settings) {
     if (interruptMode > 0) {
       uint8_t sreg = SREG;
       noInterrupts();
@@ -207,13 +200,13 @@ public:
     inTransactionFlag = 1;
     #endif
 
-    SPCR = settings.spcr;
-    SPSR = settings.spsr;
+    SPCR1 = settings.spcr1;
+    SPSR1 = settings.spsr1;
   }
 
   // Write to the SPI bus (MOSI pin) and also receive (MISO pin)
   inline static uint8_t transfer(uint8_t data) {
-    SPDR = data;
+    SPDR1 = data;
     /*
      * The following NOP introduces a small delay that can prevent the wait
      * loop form iterating when running at the maximum speed. This gives
@@ -221,46 +214,46 @@ public:
      * speeds it is unnoticed.
      */
     asm volatile("nop");
-    while (!(SPSR & _BV(SPIF))) ; // wait
-    return SPDR;
+    while (!(SPSR1 & _BV(SPIF))) ; // wait
+    return SPDR1;
   }
   inline static uint16_t transfer16(uint16_t data) {
     union { uint16_t val; struct { uint8_t lsb; uint8_t msb; }; } in, out;
     in.val = data;
-    if (!(SPCR & _BV(DORD))) {
-      SPDR = in.msb;
+    if (!(SPCR1 & _BV(DORD))) {
+      SPDR1 = in.msb;
       asm volatile("nop"); // See transfer(uint8_t) function
-      while (!(SPSR & _BV(SPIF))) ;
-      out.msb = SPDR;
-      SPDR = in.lsb;
+      while (!(SPSR1 & _BV(SPIF))) ;
+      out.msb = SPDR1;
+      SPDR1 = in.lsb;
       asm volatile("nop");
-      while (!(SPSR & _BV(SPIF))) ;
-      out.lsb = SPDR;
+      while (!(SPSR1 & _BV(SPIF))) ;
+      out.lsb = SPDR1;
     } else {
-      SPDR = in.lsb;
+      SPDR1 = in.lsb;
       asm volatile("nop");
-      while (!(SPSR & _BV(SPIF))) ;
-      out.lsb = SPDR;
-      SPDR = in.msb;
+      while (!(SPSR1 & _BV(SPIF))) ;
+      out.lsb = SPDR1;
+      SPDR1 = in.msb;
       asm volatile("nop");
-      while (!(SPSR & _BV(SPIF))) ;
-      out.msb = SPDR;
+      while (!(SPSR1 & _BV(SPIF))) ;
+      out.msb = SPDR1;
     }
     return out.val;
   }
   inline static void transfer(void *buf, size_t count) {
     if (count == 0) return;
     uint8_t *p = (uint8_t *)buf;
-    SPDR = *p;
+    SPDR1 = *p;
     while (--count > 0) {
       uint8_t out = *(p + 1);
-      while (!(SPSR & _BV(SPIF))) ;
-      uint8_t in = SPDR;
-      SPDR = out;
+      while (!(SPSR1 & _BV(SPIF))) ;
+      uint8_t in = SPDR1;
+      SPDR1 = out;
       *p++ = in;
     }
-    while (!(SPSR & _BV(SPIF))) ;
-    *p = SPDR;
+    while (!(SPSR1 & _BV(SPIF))) ;
+    *p = SPDR1;
   }
   // After performing a group of transfers and releasing the chip select
   // signal, this function allows others to access the SPI bus
@@ -296,25 +289,25 @@ public:
   // This function is deprecated.  New applications should use
   // beginTransaction() to configure SPI settings.
   inline static void setBitOrder(uint8_t bitOrder) {
-    if (bitOrder == LSBFIRST) SPCR |= _BV(DORD);
-    else SPCR &= ~(_BV(DORD));
+    if (bitOrder == LSBFIRST) SPCR1 |= _BV(DORD);
+    else SPCR1 &= ~(_BV(DORD));
   }
   // This function is deprecated.  New applications should use
   // beginTransaction() to configure SPI settings.
   inline static void setDataMode(uint8_t dataMode) {
-    SPCR = (SPCR & ~SPI_MODE_MASK) | dataMode;
+    SPCR1 = (SPCR1 & ~SPI_MODE_MASK) | dataMode;
   }
   // This function is deprecated.  New applications should use
   // beginTransaction() to configure SPI settings.
   inline static void setClockDivider(uint8_t clockDiv) {
-    SPCR = (SPCR & ~SPI_CLOCK_MASK) | (clockDiv & SPI_CLOCK_MASK);
-    SPSR = (SPSR & ~SPI_2XCLOCK_MASK) | ((clockDiv >> 2) & SPI_2XCLOCK_MASK);
+    SPCR1 = (SPCR1 & ~SPI_CLOCK_MASK) | (clockDiv & SPI_CLOCK_MASK);
+    SPSR1 = (SPSR1 & ~SPI_2XCLOCK_MASK) | ((clockDiv >> 2) & SPI_2XCLOCK_MASK);
   }
   // These undocumented functions should not be used.  SPI.transfer()
   // polls the hardware flag which is automatically cleared as the
   // AVR responds to SPI's interrupt
-  inline static void attachInterrupt() { SPCR |= _BV(SPIE); }
-  inline static void detachInterrupt() { SPCR &= ~_BV(SPIE); }
+  inline static void attachInterrupt() { SPCR1 |= _BV(SPIE); }
+  inline static void detachInterrupt() { SPCR1 &= ~_BV(SPIE); }
 
 private:
   static uint8_t initialized;
@@ -326,6 +319,6 @@ private:
   #endif
 };
 
-extern SPIClass SPI;
+extern SPI1Class SPI1;
 
 #endif
