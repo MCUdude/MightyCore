3c8961f613f5175df55adde942823cb2ac92ea85
MCUdude <mcudude@users.noreply.github.com>
2018-06-03 22:40:44 +0200

    Update header I don't think it's necessary to show the DIP pinout in the pins_arduino file anymore. The 324PB isn't even available in a DIP package

--- avr/variants/standard/pins_arduino.h
+++ avr/variants/standard/pins_arduino.h
@@ -1,52 +1,29 @@
+/***************************************************|
+|            MIGHTYCORE STANDARD PINOUT             |
+|       https://github.com/MCUdude/MightyCore       |
+|                                                   |
+| An Arduino core for:                              |
+| ATmega1284/P                                      |
+| ATmega644/A/P/PA                                  |
+| ATmega324A/P/PA/PB                                |
+| ATmega164A/P/PA                                   |
+| ATmega32/A                                        |
+| ATmega16/A                                        |
+| ATmega8535                                        |
+|                                                   |
+|***************************************************/
+
 #ifndef Pins_Arduino_h
 #define Pins_Arduino_h
 
 #include <avr/pgmspace.h>
-/*
 
-                 MIGHTYCORE STANDARD PINOUT
-         ATmega8535, ATmega16, ATmega32, ATmega164, 
-              ATmega324, ATmega644, ATmega1284
-      
-                         +---\/---+
-         LED (D 0) PB0  1|        |40  PA0 (A 0 / D24)
-             (D 1) PB1  2|        |39  PA1 (A 1 / D25)
-        INT2 (D 2) PB2  3|        |38  PA2 (A 2 / D26)
-         PWM (D 3) PB3  4|        |37  PA3 (A 3 / D27)
-    PWM* /SS (D 4) PB4  5|        |36  PA4 (A 4 / D28)
-        MOSI (D 5) PB5  6|        |35  PA5 (A 5 / D29)
- PWM** /MISO (D 6) PB6  7|        |34  PA6 (A 6 / D30)
-  PWM** /SCK (D 7) PB7  8|        |33  PA7 (A 7 / D31)
-                   RST  9|        |32  AREF
-                   VCC 10|        |31  GND 
-                   GND 11|        |30  AVCC
-                 XTAL2 12|        |29  PC7 (D 23)
-                 XTAL1 13|        |28  PC6 (D 22)
-        RX0 (D 8)  PD0 14|        |27  PC5 (D 21) TDI
-        TX0 (D 9)  PD1 15|        |26  PC4 (D 20) TDO
- (RX1)/INT0 (D 10) PD2 16|        |25  PC3 (D 19) TMS
- (TX1)/INT1 (D 11) PD3 17|        |24  PC2 (D 18) TCK
-        PWM (D 12) PD4 18|        |23  PC1 (D 17) SDA
-        PWM (D 13) PD5 19|        |22  PC0 (D 16) SCL
-       PWM* (D 14) PD6 20|        |21  PD7 (D 15) PWM
-                         +--------+
-       PWM: ATmega8535/16/32/164/324/644/1284
-       PWM*: ATmega164/324/644/1284
-       PWM**: ATmega1284
-       PCINT ONLY ON ATmega164/324/644/1284
-   
-PCINT15-8: D7-0    : bit 1
-PCINT31-24: D15-8  : bit 3
-PCINT23-16: D23-16 : bit 2
-PCINT7-0: D31-24   : bit 0
-*/
-
+#define STANDARD_PINOUT
 #if defined(__AVR_ATmega324PB__)
   #define NUM_DIGITAL_PINS          39
 #else
   #define NUM_DIGITAL_PINS          32
 #endif
-#define STANDARD_PINOUT
 #define NUM_ANALOG_INPUTS           8
 #define EXTERNAL_NUM_INTERRUPTS     3
 #define analogInputToDigitalPin(p)  ((p < NUM_ANALOG_INPUTS) ? (p) + 24 : -1)
@@ -490,5 +467,3 @@ defined(__AVR_ATmega324P__) || defined(__AVR_ATmega324PA__)
 
 
 #endif // Pins_Arduino_h
-
-// vim:ai:cin:sts=2 sw=2 ft=cpp
