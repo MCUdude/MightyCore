0206da2123bac744d89d7eb033589ee9a7ef50b4
MCUdude <mcudude@users.noreply.github.com>
2018-12-17 21:32:18 +0100

    Squashed 'avr/cores/MCUdude_corefiles/' changes from 3c376f7d..2d9c31b8
    2d9c31b8 Replace tabs with spaces
    28277ab3 Replace ATmega644 with ATmega644A 644A is binary compatible with 644, but has two UARTS
    edf8d239 Fix INT2 issue on ATmega8515/162 Turns out that the registers to control INT2 on 8515/162 is different than every other AVR out there.
    
    git-subtree-dir: avr/cores/MCUdude_corefiles
    git-subtree-split: 2d9c31b88a777f66d22a049786868ad9ee881423

--- WInterrupts.c
+++ WInterrupts.c
@@ -190,8 +190,12 @@ void attachInterrupt(uint8_t interruptNum, void (*userFunc)(void), int mode)
           GICR |= (1 << INT1);
           break;
         case 2:
-          #if defined(INT2) // Will exclude ATmega8, since it doesn't have INT2
-          MCUCSR = (MCUCSR & ~((1 << ISC2))) | ((mode & 0x01) << ISC2);
+          #if defined(INT2) 
+          #if defined(EMCUCR) 
+          EMCUCR = (EMCUCR & ~((1 << ISC2))) | ((mode & 0x01) << ISC2); // ATmega8515/162
+          #else
+          MCUCSR = (MCUCSR & ~((1 << ISC2))) | ((mode & 0x01) << ISC2); // ATmega8535/16/32
+          #endif
           GICR |= (1 << INT2);          
           #endif
           break; 
@@ -199,7 +203,7 @@ void attachInterrupt(uint8_t interruptNum, void (*userFunc)(void), int mode)
 // ATmega164A/P, ATmega324A/P/PA, ATmega644/P, ATmega1284/P      
       #elif defined(__AVR_ATmega164A__) || defined(__AVR_ATmega164P__) || defined(__AVR_ATmega324A__) \
       || defined(__AVR_ATmega324P__) || defined(__AVR_ATmega324PA__) || defined(__AVR_ATmega324PB__) \
-      || defined(__AVR_ATmega644__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega1284__) \
+      || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega1284__) \
       || defined(__AVR_ATmega1284P__)
         case 0:
           EICRA = (EICRA & ~((1 << ISC00) | (1 << ISC01))) | (mode << ISC00);
