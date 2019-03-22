a09f21bba413d5dfe924b1cc838bdfb3cb0a0e8d
MCUdude <hansibull@gmail.com>
2019-03-22 23:09:35 +0100

    Add extra 324PB UART macros This will make compatibility with the rest of the 324 family better. First pointed out in #134

--- avr/variants/bobuino/pins_arduino.h
+++ avr/variants/bobuino/pins_arduino.h
@@ -183,73 +183,6 @@ const uint8_t digital_pin_to_pcint[NUM_DIGITAL_PINS] =
 
 #endif
 
-#if defined(__AVR_ATmega164PA__) || defined(__AVR_ATmega324A__) || defined(__AVR_ATmega324P__)\
-|| defined(__AVR_ATmega324PA__)
-/**** Needed to get the SD library to work.
-Missing definitions in the iom164.h/iom324.h file ****/
-#define SPR0 SPR00
-#define SPR1 SPR10
-#define CPHA CPHA0
-#define CPOL CPOL0
-#define MSTR MSTR0
-#define DORD DORD0
-#define SPE SPE0
-#define SPIE SPIE0
-#define SPSR SPSR0
-#define SPI2X SPI2X0
-#define WCOL WCOL0
-#define SPIF SPIF0
-#define SPCR SPCR0
-#define SPDR SPDR0
-#endif
-
-// Missing definitions in iom324pb.h file
-#if defined(__AVR_ATmega324PB__)
-// i2c
-#define TWI_vect TWI0_vect
-#define TWI_vect_num TWI0_vect_num
-#define TWBR TWBR0
-#define TWSR TWSR0
-#define TWS3 TWS03
-#define TWS4 TWS04
-#define TWS5 TWS05
-#define TWS6 TWS06
-#define TWS7 TWS07
-#define TWAR TWAR0
-#define TWDR TWDR0
-#define TWD0 0
-#define TWD1 1
-#define TWD2 2
-#define TWD3 3
-#define TWD4 4
-#define TWD5 5
-#define TWD6 6
-#define TWD7 7
-#define TWCR  TWCR0
-#define TWAMR TWAMR0
-#define TWAM0 TWAM00
-#define TWAM1 TWAM01
-#define TWAM2 TWAM02
-#define TWAM3 TWAM03
-#define TWAM4 TWAM04
-#define TWAM5 TWAM05
-#define TWAM6 TWAM06
-
-// SPI
-#define SPI_STC_vect SPI0_STC_vect
-#define SPI_STC_vect_num SPI0_STC_vect_num
-#define SPCR SPCR0
-#define SPSR SPSR0
-#define SPDR SPDR0
-#define SPDRB0 0
-#define SPDRB1 1
-#define SPDRB2 2
-#define SPDRB3 3
-#define SPDRB4 4
-#define SPDRB5 5
-#define SPDRB6 6
-#define SPDRB7 7
-#endif
 
 #ifdef ARDUINO_MAIN
 
@@ -551,6 +484,148 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
 
 #endif // ARDUINO_MAIN
 
-#endif // Pins_Arduino_h
 
-// vim:ai:cin:sts=2 sw=2 ft=cpp
+// Missing definitions in iom164pa.h/iom324a.h/iom324p.h/iom324pa.h
+#if defined(__AVR_ATmega164PA__) || defined(__AVR_ATmega324A__) || defined(__AVR_ATmega324P__)\
+|| defined(__AVR_ATmega324PA__)
+  #define SPSR SPSR0
+  #define SPDR SPDR0
+  #define SPCR SPCR0
+  #define SPR0 SPR00
+  #define SPR1 SPR10
+  #define CPHA CPHA0
+  #define CPOL CPOL0
+  #define MSTR MSTR0
+  #define DORD DORD0
+  #define SPE  SPE0
+  #define SPIE SPIE0
+  #define SPI2X SPI2X0
+  #define WCOL WCOL0
+  #define SPIF SPIF0
+#endif
+
+
+// Missing definitions in iom324pb.h file + better backwards compatibility
+#if defined(__AVR_ATmega324PB__)
+  // USART
+  #define MPCM0   MPCM
+  #define U2X0    U2X
+  #define UPE0    UPE
+  #define DOR0    DOR
+  #define FE0     FE
+  #define UDRE0   UDRE
+  #define TXC0    TXC
+  #define RXC0    RXC
+  #define TXB80   TXB8
+  #define RXB80   RXB8
+  #define UCSZ02  UCSZ2
+  #define TXEN0   TXEN
+  #define RXEN0   RXEN
+  #define UDRIE0  UDRIE
+  #define TXCIE0  TXCIE
+  #define RXCIE0  RXCIE
+  #define UCPOL0  UCPOL
+  #define UCSZ00  UCSZ0
+  #define UCSZ01  UCSZ1
+  #define USBS0   USBS
+  #define UPM00   UPM0
+  #define UPM01   UPM1
+  #define UMSEL00 UMSEL0
+  #define UMSEL01 UMSEL1  
+  #define MPCM1   MPCM
+  #define U2X1    U2X
+  #define UPE1    UPE
+  #define DOR1    DOR
+  #define FE1     FE
+  #define UDRE1   UDRE
+  #define TXC1    TXC
+  #define RXC1    RXC
+  #define TXB81   TXB8
+  #define RXB81   RXB8
+  #define UCSZ12  UCSZ2
+  #define TXEN1   TXEN
+  #define RXEN1   RXEN
+  #define UDRIE1  UDRIE
+  #define TXCIE1  TXCIE
+  #define RXCIE1  RXCIE
+  #define UCPOL1  UCPOL
+  #define UCSZ10  UCSZ0
+  #define UCSZ11  UCSZ1
+  #define USBS1   USBS
+  #define UPM10   UPM0
+  #define UPM11   UPM1
+  #define UMSEL10 UMSEL0
+  #define UMSEL11 UMSEL1 
+  #define MPCM2   MPCM
+  #define U2X2    U2X
+  #define UPE2    UPE
+  #define DOR2    DOR
+  #define FE2     FE
+  #define UDRE2   UDRE
+  #define TXC2    TXC
+  #define RXC2    RXC
+  #define TXB82   TXB8
+  #define RXB82   RXB8
+  #define UCSZ22  UCSZ2
+  #define TXEN2   TXEN
+  #define RXEN2   RXEN
+  #define UDRIE2  UDRIE
+  #define TXCIE2  TXCIE
+  #define RXCIE2  RXCIE
+  #define UCPOL2  UCPOL
+  #define UCSZ20  UCSZ0
+  #define UCSZ21  UCSZ1
+  #define USBS2   USBS
+  #define UPM20   UPM0
+  #define UPM21   UPM1
+  #define UMSEL20 UMSEL0
+  #define UMSEL21 UMSEL1
+  
+  // i2c
+  #define TWI_vect TWI0_vect
+  #define TWI_vect_num TWI0_vect_num
+  #define TWBR TWBR0
+  #define TWSR TWSR0
+  #define TWS3 TWS03
+  #define TWS4 TWS04
+  #define TWS5 TWS05
+  #define TWS6 TWS06
+  #define TWS7 TWS07
+  #define TWAR TWAR0
+  #define TWDR TWDR0
+  #define TWD0 0
+  #define TWD1 1
+  #define TWD2 2
+  #define TWD3 3
+  #define TWD4 4
+  #define TWD5 5
+  #define TWD6 6
+  #define TWD7 7
+  #define TWCR  TWCR0
+  #define TWAMR TWAMR0
+  #define TWAM0 TWAM00
+  #define TWAM1 TWAM01
+  #define TWAM2 TWAM02
+  #define TWAM3 TWAM03
+  #define TWAM4 TWAM04
+  #define TWAM5 TWAM05
+  #define TWAM6 TWAM06
+
+  // SPI
+  #define SPI_STC_vect SPI0_STC_vect
+  #define SPI_STC_vect_num SPI0_STC_vect_num
+  #define SPCR SPCR0
+  #define SPSR SPSR0
+  #define SPDR SPDR0
+  #define SPDRB0 0
+  #define SPDRB1 1
+  #define SPDRB2 2
+  #define SPDRB3 3
+  #define SPDRB4 4
+  #define SPDRB5 5
+  #define SPDRB6 6
+  #define SPDRB7 7
+
+#endif // 324PB defs
+
+#endif // Pins_Arduino_h
--- avr/variants/standard/pins_arduino.h
+++ avr/variants/standard/pins_arduino.h
@@ -452,8 +452,82 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
 #endif
 
 
-// Missing definitions in iom324pb.h file
+// Missing definitions in iom324pb.h file + better backwards compatibility
 #if defined(__AVR_ATmega324PB__)
+  // USART
+  #define MPCM0   MPCM
+  #define U2X0    U2X
+  #define UPE0    UPE
+  #define DOR0    DOR
+  #define FE0     FE
+  #define UDRE0   UDRE
+  #define TXC0    TXC
+  #define RXC0    RXC
+  #define TXB80   TXB8
+  #define RXB80   RXB8
+  #define UCSZ02  UCSZ2
+  #define TXEN0   TXEN
+  #define RXEN0   RXEN
+  #define UDRIE0  UDRIE
+  #define TXCIE0  TXCIE
+  #define RXCIE0  RXCIE
+  #define UCPOL0  UCPOL
+  #define UCSZ00  UCSZ0
+  #define UCSZ01  UCSZ1
+  #define USBS0   USBS
+  #define UPM00   UPM0
+  #define UPM01   UPM1
+  #define UMSEL00 UMSEL0
+  #define UMSEL01 UMSEL1  
+  #define MPCM1   MPCM
+  #define U2X1    U2X
+  #define UPE1    UPE
+  #define DOR1    DOR
+  #define FE1     FE
+  #define UDRE1   UDRE
+  #define TXC1    TXC
+  #define RXC1    RXC
+  #define TXB81   TXB8
+  #define RXB81   RXB8
+  #define UCSZ12  UCSZ2
+  #define TXEN1   TXEN
+  #define RXEN1   RXEN
+  #define UDRIE1  UDRIE
+  #define TXCIE1  TXCIE
+  #define RXCIE1  RXCIE
+  #define UCPOL1  UCPOL
+  #define UCSZ10  UCSZ0
+  #define UCSZ11  UCSZ1
+  #define USBS1   USBS
+  #define UPM10   UPM0
+  #define UPM11   UPM1
+  #define UMSEL10 UMSEL0
+  #define UMSEL11 UMSEL1 
+  #define MPCM2   MPCM
+  #define U2X2    U2X
+  #define UPE2    UPE
+  #define DOR2    DOR
+  #define FE2     FE
+  #define UDRE2   UDRE
+  #define TXC2    TXC
+  #define RXC2    RXC
+  #define TXB82   TXB8
+  #define RXB82   RXB8
+  #define UCSZ22  UCSZ2
+  #define TXEN2   TXEN
+  #define RXEN2   RXEN
+  #define UDRIE2  UDRIE
+  #define TXCIE2  TXCIE
+  #define RXCIE2  RXCIE
+  #define UCPOL2  UCPOL
+  #define UCSZ20  UCSZ0
+  #define UCSZ21  UCSZ1
+  #define USBS2   USBS
+  #define UPM20   UPM0
+  #define UPM21   UPM1
+  #define UMSEL20 UMSEL0
+  #define UMSEL21 UMSEL1
+  
   // i2c
   #define TWI_vect TWI0_vect
   #define TWI_vect_num TWI0_vect_num
@@ -499,7 +573,6 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] =
   #define SPDRB6 6
   #define SPDRB7 7
 
-#endif
-
+#endif // 324PB defs
 
 #endif // Pins_Arduino_h
